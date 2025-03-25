36e17d61d84d7baea04f9f87507ff2d396d5bac6
Achraf EL KHANDOULI <achraf.contacts@gmail.com>
2025-03-25 20:59:29 +0100

    fix initial data import

--- backend/src/main/java/com/fox/website/config/SecurityConfig.java
+++ backend/src/main/java/com/fox/website/config/SecurityConfig.java
@@ -85,7 +85,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2-console/**");
 
     }
 
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-
 --  PRODUCTS
 INSERT INTO product(product_id, product_name, product_price, product_icon, product_description) VALUES(1, 'FXG SHIRT', 200, 'https://personal-images-store.s3.us-east-2.amazonaws.com/fxg-shirt.jpg', 'FXG Main Jersey gaming shirt');
 INSERT INTO product(product_id, product_name, product_price, product_icon, product_description) VALUES(2, 'FXG Akashi', 200, 'https://personal-images-store.s3.us-east-2.amazonaws.com/akashi-shirt.jpg', 'FXG Akashi Jersey gaming shirt');

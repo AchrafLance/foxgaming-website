01d3b6262f96cf0b5d65cb6139f00077e955daed
Achraf EL KHANDOULI <achraf.contacts@gmail.com>
2025-03-29 20:34:27 +0100

    v16

--- frontend/src/app/_guards/auth-guard.ts
+++ frontend/src/app/_guards/auth-guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "../services/auth-service";
 
@@ -9,7 +9,7 @@ import { AuthService } from "../services/auth-service";
     providedIn: "root"
 })
 
-export class AuthGuard implements CanActivate{
+export class AuthGuard {
 
     constructor(private authService: AuthService, 
         private router: Router){
--- frontend/src/app/services/resolvers/cart-resolver.ts
+++ frontend/src/app/services/resolvers/cart-resolver.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { Order } from "src/app/models/order";
 import { CartService } from "../cart-service";
 
 @Injectable({providedIn:"root"})
-export class CartResolver implements Resolve<Order[]>{
+export class CartResolver {
     constructor(private cartService: CartService){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Order[]> | Promise<Order[]> | Order[] {

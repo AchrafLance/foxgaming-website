47cfd9b9950c90b9b1cdaa999201fc79b34331b7
Achraf EL KHANDOULI <achraf.contacts@gmail.com>
2025-04-02 20:37:59 +0200

    fix shop

--- frontend/src/app/main/shop/product-details/product-details.component.ts
+++ frontend/src/app/main/shop/product-details/product-details.component.ts
@@ -2,69 +2,80 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from 'src/app/models/order';
 import { ProductInfo } from 'src/app/models/productInfo';
-import { Router} from "@angular/router"
+import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart-service';
 import { WishlistService } from 'src/app/services/wishlist-service';
 import { ProductService } from 'src/app/services/product-service';
 import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService} from 'ngx-spinner'
+import { NgxSpinnerService } from 'ngx-spinner';
+import { UtilService } from '../../../services/util-service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  currentProduct!: ProductInfo;
 
-  currentProduct: any; 
-
-  sizeS = false; 
-  sizeM = false; 
-  sizeL = false; 
-  sizeXL = false; 
-  size2XL = false; 
-  selectedSize = "S"; 
+  sizeS = false;
+  sizeM = false;
+  sizeL = false;
+  sizeXL = false;
+  size2XL = false;
+  selectedSize = 'S';
   selectedQuantity = 1;
-  currentRoute:any; 
-  facebookSharer = "https://www.facebook.com/sharer/sharer.php?u="
-  twitterSharer = "https://twitter.com/intent/tweet?text="; 
-  productId: number; 
-  
-  constructor( private activatedRoute: ActivatedRoute, 
-    private router: Router, 
+  currentRoute: any;
+  facebookSharer = 'https://www.facebook.com/sharer/sharer.php?u=';
+  twitterSharer = 'https://twitter.com/intent/tweet?text=';
+  productId: number;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
     private cartService: CartService,
-    private wishlistService: WishlistService, 
+    private wishlistService: WishlistService,
     private productService: ProductService,
-    private toastrService: ToastrService, 
-    private spinnerService: NgxSpinnerService) { }
+    private toastrService: ToastrService,
+    private utilService: UtilService,
+  ) {}
 
   ngOnInit(): void {
-    this.spinnerService.show(); 
-    setTimeout(()=>{
-      this.spinnerService.hide()
-    }, 1000)
-    this.activatedRoute.params.subscribe(params => {
-      this.productId = params["id"];       
-    }); 
+    this.activatedRoute.params.subscribe((params) => {
+      this.productId = params['id'];
+      this.currentProduct = this.utilService.getProductById(this.productId);
+    });
 
-    this.productService.getOneProduct(this.productId).subscribe(data => {
-      if(data){
-        this.currentProduct = data; 
-        this.currentRoute = "http://www.localhost.com" + this.router.url; 
-        this.facebookSharer =  this.facebookSharer +   this.currentRoute 
-        this.twitterSharer = this.twitterSharer + this.currentProduct.productName + "\r\n " + this.currentRoute;       }
-    })
+    this.productService
+      .getOneProduct(this.productId)
+      .subscribe((data: ProductInfo) => {
+        if (data) {
+          this.currentRoute = 'http://www.localhost.com' + this.router.url;
+          this.facebookSharer = this.facebookSharer + this.currentRoute;
+          this.twitterSharer =
+            this.twitterSharer +
+            this.currentProduct.productName +
+            '\r\n ' +
+            this.currentRoute;
+        }
+      });
+  }
 
-    };
-
-    onSelectSizeS(){
+  onSelectSizeS() {
     this.sizeS = !this.sizeS;
     this.sizeM = false;
     this.sizeL = false;
     this.sizeXL = false;
     this.size2XL = false;
-    this.selectedSize = "S"; 
-    if(!this.size2XL && !this.sizeXL && !this.sizeS && !this.sizeM && !this.sizeL){
-      this.selectedSize = ""; 
+    this.selectedSize = 'S';
+    if (
+      !this.size2XL &&
+      !this.sizeXL &&
+      !this.sizeS &&
+      !this.sizeM &&
+      !this.sizeL
+    ) {
+      this.selectedSize = '';
     }
   }
 
@@ -74,9 +85,15 @@ export class ProductDetailsComponent implements OnInit {
     this.sizeL = false;
     this.sizeXL = false;
     this.size2XL = false;
-    this.selectedSize = "M"
-    if(!this.size2XL && !this.sizeXL && !this.sizeS && !this.sizeM && !this.sizeL){
-      this.selectedSize = ""; 
+    this.selectedSize = 'M';
+    if (
+      !this.size2XL &&
+      !this.sizeXL &&
+      !this.sizeS &&
+      !this.sizeM &&
+      !this.sizeL
+    ) {
+      this.selectedSize = '';
     }
   }
 
@@ -86,9 +103,15 @@ export class ProductDetailsComponent implements OnInit {
     this.sizeM = false;
     this.sizeXL = false;
     this.size2XL = false;
-    this.selectedSize = "L"
-    if(!this.size2XL && !this.sizeXL && !this.sizeS && !this.sizeM && !this.sizeL){
-      this.selectedSize = ""; 
+    this.selectedSize = 'L';
+    if (
+      !this.size2XL &&
+      !this.sizeXL &&
+      !this.sizeS &&
+      !this.sizeM &&
+      !this.sizeL
+    ) {
+      this.selectedSize = '';
     }
   }
 
@@ -98,59 +121,69 @@ export class ProductDetailsComponent implements OnInit {
     this.sizeM = false;
     this.sizeL = false;
     this.size2XL = false;
-    this.selectedSize = "XL"
-    if(!this.size2XL && !this.sizeXL && !this.sizeS && !this.sizeM && !this.sizeL){
-      this.selectedSize = ""; 
+    this.selectedSize = 'XL';
+    if (
+      !this.size2XL &&
+      !this.sizeXL &&
+      !this.sizeS &&
+      !this.sizeM &&
+      !this.sizeL
+    ) {
+      this.selectedSize = '';
     }
   }
 
   onSelectSize2XL() {
-    this.size2XL = !this.size2XL
+    this.size2XL = !this.size2XL;
     this.sizeS = false;
     this.sizeM = false;
     this.sizeL = false;
     this.sizeXL = false;
-    this.selectedSize = "2XL"
-    if(!this.size2XL && !this.sizeXL && !this.sizeS && !this.sizeM && !this.sizeL){
-      this.selectedSize = ""; 
+    this.selectedSize = '2XL';
+    if (
+      !this.size2XL &&
+      !this.sizeXL &&
+      !this.sizeS &&
+      !this.sizeM &&
+      !this.sizeL
+    ) {
+      this.selectedSize = '';
     }
   }
 
-  onDecrementQty(){
-    if(this.selectedQuantity >= 2){
-      this.selectedQuantity--; 
+  onDecrementQty() {
+    if (this.selectedQuantity >= 2) {
+      this.selectedQuantity--;
     }
   }
 
-  onIncrementQty(){
-    if(this.selectedQuantity < 20){
-    this.selectedQuantity++; 
+  onIncrementQty() {
+    if (this.selectedQuantity < 20) {
+      this.selectedQuantity++;
     }
   }
 
-  onAddToCard(product: ProductInfo){
-    let order= new Order(); 
-    order.quantity = this.selectedQuantity; 
-    order.size = this.selectedSize; 
-    order.product = product; 
-    this.cartService.addItemToCart(order).subscribe(data=>{
-      if(data){
+  onAddToCard(product: ProductInfo) {
+    let order = new Order();
+    order.quantity = this.selectedQuantity;
+    order.size = this.selectedSize;
+    order.product = product;
+    this.cartService.addItemToCart(order).subscribe((data) => {
+      if (data) {
         this.cartService.cartItems.push(order);
-        let cartCount = this.cartService.cartCount.value; 
+        let cartCount = this.cartService.cartCount.value;
         this.cartService.cartCount.next(++cartCount);
-        this.toastrService.success("item addedd to cart", "Success")
-    
+        this.toastrService.success('item addedd to cart', 'Success');
       }
-    }); 
+    });
   }
 
-  onAddToWishList(product: ProductInfo){
-    this.wishlistService.addItemToWishlist(product).subscribe(data => {
-      if(data){
+  onAddToWishList(product: ProductInfo) {
+    this.wishlistService.addItemToWishlist(product).subscribe((data) => {
+      if (data) {
         this.wishlistService.wishlistItems.push(product);
-        this.toastrService.success("item addedd to wishlist", "Success")
+        this.toastrService.success('item addedd to wishlist', 'Success');
       }
-    })
+    });
   }
-
 }
--- frontend/src/app/services/util-service.ts
+++ frontend/src/app/services/util-service.ts
@@ -1,13 +1,41 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ProductInfo } from '../models/productInfo';
 
 @Injectable({
-    providedIn:"root"
+  providedIn: 'root',
 })
-export class UtilService{
-    carouselBg:BehaviorSubject<boolean>; // index-1 for carousel when sidebar opens
-    constructor(){
-        this.carouselBg = new BehaviorSubject(false); 
-    }
+export class UtilService {
+  mockProductList: ProductInfo[] = [
+    {
+      productId: 1,
+      productName: 'FXG SHIRT',
+      productPrice: 200,
+      productDescription: 'FXG Main Jersey gaming shirt',
+      productIcon: 'assets/fxg-shirt.jpg',
+    },
+    {
+      productId: 2,
+      productName: 'FXG Akashi',
+      productPrice: 200,
+      productDescription: 'FXG Akashi Jersey gaming shirt',
+      productIcon: 'assets/akashi-shirt.jpg',
+    },
+    {
+      productId: 3,
+      productName: 'FXG Akazan',
+      productPrice: 200,
+      productDescription: 'FXG Akazan Jersey gaming shirt',
+      productIcon: 'assets/akazan-shirt.jpg',
+    },
+  ];
 
+  carouselBg: BehaviorSubject<boolean>; // index-1 for carousel when sidebar opens
+  constructor() {
+    this.carouselBg = new BehaviorSubject(false);
+  }
+
+  getProductById(id: number): ProductInfo | undefined {
+    return this.mockProductList.find((product) => product.productId == id);
+  }
 }
